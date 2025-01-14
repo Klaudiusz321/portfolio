cd6deccc4e8805f7f6b65e3c51f0e171d8008dea
Klaudiusz321 <claudiuswebdesign@gmail.com>
2025-01-14 19:18:02 +0100

    popawione expriance

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -6,18 +6,18 @@ const Footer = () => {
       <div className='text-white-500 flex gap-2'>
         <p>Terms & Conditions</p>
         <p>|</p>
-        <p>Privacy Politicy</p>
+        <p>Privacy Policy</p>
       </div>
       <div className='flex gap-3'>
-        <div className='social-icon' href="https://github.com/Klaudiusz321"> 
-          <img  src="/assets/github.svg" alt="github" className='w-1/2 h-1/2'  />
-        </div>
-        <div className='social-icon' href="https://x.com/Claudiusdesign"> 
-          <img  src="/assets/twitter.svg" alt="twitter" className='w-1/2 h-1/2'  />
-        </div>
-        <div className='social-icon' href="https://www.instagram.com/klaudiusz_sroka/"> 
-          <img  src="/assets/instagram.svg" alt="insagram" className='w-1/2 h-1/2' />
-        </div>
+        <a href="https://github.com/Klaudiusz321" className='social-icon' target="_blank" rel="noopener noreferrer">
+          <img src="/assets/github.svg" alt="github" className='w-1/2 h-1/2' />
+        </a>
+        <a href="https://x.com/Claudiusdesign" className='social-icon' target="_blank" rel="noopener noreferrer">
+          <img src="/assets/twitter.svg" alt="twitter" className='w-1/2 h-1/2' />
+        </a>
+        <a href="https://www.instagram.com/klaudiusz_sroka/" className='social-icon' target="_blank" rel="noopener noreferrer">
+          <img src="/assets/instagram.svg" alt="instagram" className='w-1/2 h-1/2' />
+        </a>
       </div>
       <p className='text-white-500'>© 2024 Klaudiusz. All rights reserved.</p>
     </section>
